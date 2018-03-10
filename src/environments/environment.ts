b079502de9f55b68135920541302b2df63758307
prakharsr <prakhar03sharma@gmail.com>
2018-03-10 10:11:06 +0530

    mfix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@
 export const environment = {
   production: false,
   razorPay: "rzp_test_glA246D8rCFOVc",
-  apiUrl:  "http://b9527fed.ngrok.io/api",
-  uploadsBaseUrl:  "http://b9527fed.ngrok.io",
+  apiUrl:  "http://b69f5777.ngrok.io/api",
+  uploadsBaseUrl:  "http://b69f5777.ngrok.io",
   // apiUrl:  "http://localhost:8080/api",
   // uploadsBaseUrl:  "http://localhost:8080"
 };
