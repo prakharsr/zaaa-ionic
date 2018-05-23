9073e1eece765e3ee8432e93bde570bd6fe79409
Prakhar Sharma <prakhar03sharma@gmail.com>
2018-05-23 14:27:32 +0530

    error..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { IonicApp, IonicErrorHandler, IonicModule, NavController } from 'ionic-a
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
-import { AppComponent } from './components/app/app.component';
+import { AppComponent } from '@aaman/main/home/app.component';
 import { ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -15,29 +15,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { BaseModule } from './base.module';
-import { AdminModule } from './admin/admin.module';
-import { AppRoutingModule } from './/app-routing.module';
-import { DirectoryModule } from './directory/directory.module';
-import { CoUsersModule } from './co-users/co-users.module';
-import { RateCardModule } from './rate-card/rate-card.module';
-import { ReleaseOrderModule } from './release-order/release-order.module';
-
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { PhoneVerifyComponent } from './components/phone-verify/phone-verify.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { ProfileViewComponent } from './components/profile-view/profile-view.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FirmProfileViewComponent } from './components/firm-profile-view/firm-profile-view.component';
-import { ChangePswComponent } from './components/change-psw/change-psw.component';
-import { ForgotPswComponent } from './components/forgot-psw/forgot-psw.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { LoaderComponent } from './components/loader/loader.component';
-import { InvoiceModule } from './invoice/invoice.module';
+import { AdminModule } from '@aaman/main/admin.module';
+import { DirectoryModule } from '@aaman/dir/directory.module';
+import { CoUsersModule } from '@aaman/couser/co-users.module';
+import { RateCardModule } from '@aaman/ratecard/rate-card.module';
+import { ReleaseOrderModule } from '@aaman/releaseorder/release-order.module';
+import { InvoiceModule } from '@aaman/invoice/invoice.module';
 import { ReceiptsModule } from '@aaman/receipts/receipts.module';
 import { AccountsModule } from '@aaman/accounts/accounts.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { HomeComponent } from '@aaman/main/home/home.component';
+import { PhoneVerifyComponent } from '@aaman/main/phone-verify/phone-verify.component';
+import { NavbarComponent } from '@aaman/main/navbar/navbar.component';
+import { LoginComponent } from '@aaman/main/login/login.component';
+import { RegisterComponent } from '@aaman/main/register/register.component';
+import { ProfileViewComponent } from '@aaman/main/profile-view/profile-view.component';
+import { DashboardComponent } from '@aaman/main/dashboard/dashboard.component';
+import { FirmProfileViewComponent } from '@aaman/main/firm-profile-view/firm-profile-view.component';
+import { ChangePswComponent } from '@aaman/main/change-psw/change-psw.component';
+import { ForgotPswComponent } from '@aaman/main/forgot-psw/forgot-psw.component';
+import { NotFoundComponent } from '@aaman/main/not-found/not-found.component';
+import { ResetPasswordComponent } from '@aaman/main/reset-password/reset-password.component';
+import { LoaderComponent } from '@aaman/main/loader/loader.component';
 
 @NgModule({
   declarations: [
